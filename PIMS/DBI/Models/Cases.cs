37ca89c7957ab5a753870a6b895f831b038e3486
Jeb <jeb.watson@cepedasystems.com>
2017-01-23 13:49:47 -0600

    Add cases model

--- PIMS/DBI/Models/Cases.cs
+++ PIMS/DBI/Models/Cases.cs
@@ -6,5 +6,27 @@ namespace DBI
 {
     class Cases
     {
+        private const int _unassigned = -1;
+
+        public int caseId { get; set; }
+        public int supervisingDoctorId { get; set; }
+        public int approvedVisitorCount { get; set; }
+        public int patientId { get; set; }
+
+        public Cases()
+        {
+            caseId = _unassigned;
+            supervisingDoctorId = _unassigned;
+            approvedVisitorCount = _unassigned;
+            patientId = _unassigned;
+        }
+
+        public Cases(int valCaseId, int valSupervisingDoctorId, int valApprovedVisitorCount, int valPatientId)
+        {
+            caseId = valCaseId;
+            supervisingDoctorId = valSupervisingDoctorId;
+            approvedVisitorCount = valApprovedVisitorCount;
+            patientId = valPatientId;
+        }
     }
 }
