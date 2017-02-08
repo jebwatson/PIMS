670cc6205e808aa36efa7bdd9e86de35ae5e0b0b
Jeb <jeb.watson@cepedasystems.com>
2017-02-08 10:20:04 -0600

    Add charges table + tests
    -Add the charges table implementation
    -Add the test cases for the charges table

--- PIMS/DBI/Models/Charges.cs
+++ PIMS/DBI/Models/Charges.cs
@@ -6,5 +6,46 @@ namespace DBI
 {
     class Charges
     {
+        private const int _unassigned = -1;
+
+        public int chargeId { get; set; }
+        public string description { get; set; }
+        public int amountDollars { get; set; }
+        public int amountCents { get; set; }
+        public DateTime dateCharged { get; set; }
+        public DateTime dateDue { get; set; }
+        public int procedureId { get; set; }
+        public int officeStaffId { get; set; }
+        public int patientId { get; set; }
+        public int caseId { get; set; }
+
+        public Charges()
+        {
+            chargeId = _unassigned;
+            description = string.Empty;
+            amountDollars = _unassigned;
+            amountCents = _unassigned;
+            dateCharged = DateTime.Now;
+            dateDue = DateTime.Now;
+            procedureId = _unassigned;
+            officeStaffId = _unassigned;
+            patientId = _unassigned;
+            caseId = _unassigned;
+        }
+
+        public Charges(int chargeId, string description, int amountDollars, int amountCents, DateTime dateCharged, DateTime dateDue, int procedureId,
+            int officeStaffId, int patientId, int caseId)
+        {
+            this.chargeId = chargeId;
+            this.description = description;
+            this.amountDollars = amountDollars;
+            this.amountCents = amountCents;
+            this.dateCharged = dateCharged;
+            this.dateDue = dateDue;
+            this.procedureId = procedureId;
+            this.officeStaffId = officeStaffId;
+            this.patientId = patientId;
+            this.caseId = caseId;
+        }
     }
 }
--- PIMS/DBI/Tables/ChargesTable.cs
+++ PIMS/DBI/Tables/ChargesTable.cs
@@ -1,10 +1,255 @@
-﻿using System;
+﻿using DBI.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace DBI
 {
     class ChargesTable
     {
+        public const string theTable = "charges";
+
+        public List<Charges> ItemList { get; set; } = new List<Charges>();
+
+        /// <summary>
+        /// Delete all records from the table.
+        /// </summary>
+        public void ClearTable()
+        {
+            if (CountRows() == 0)
+                return;
+
+            string myCommand = "DELETE FROM " + theTable;
+            QueryExecutor.ExecuteSqlNonQuery(myCommand);
+        }
+
+        /// <summary>
+        /// Delete any records with the admissionId specified.
+        /// </summary>
+        /// <param name="chargeId"></param>
+        public void ClearTableById(int chargeId)
+        {
+            using (SqlConnection myConnection = ConnectionsManager.GetNewConnection())
+            {
+                string myQuery = "DELETE FROM " + theTable +
+                    " WHERE " +
+                    "chargeId = @chargeId";
+
+                SqlCommand myCommand = new SqlCommand(myQuery, myConnection);
+
+                myCommand.Parameters.AddWithValue("@chargeId", chargeId);
+
+                myCommand.ExecuteNonQuery();
+                myConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Get a count of all records in the table.
+        /// </summary>
+        /// <returns></returns>
+        public int CountRows()
+        {
+            using (SqlConnection myConnection = ConnectionsManager.GetNewConnection())
+            {
+                string myQuery = "SELECT COUNT(*) FROM " + theTable;
+                return QueryExecutor.ExecuteSqlQueryScalar(myQuery, myConnection);
+            } // using
+        } // CountRows
+
+        /// <summary>
+        /// Read all records from the table and save them in the ItemList
+        /// as Admission objects.
+        /// </summary>
+        public void ReadList()
+        {
+            ItemList.Clear();   // ensure that the itemlist is empty so we don't get duplicates
+
+            string myQuery = "SELECT * FROM " + theTable;
+            DataSet dsObject = QueryExecutor.ExecuteSqlQuery(myQuery);
+
+            if (dsObject != null && dsObject.Tables[0].Rows.Count > 0)
+            {
+                DataTable dtObject = dsObject.Tables[0];    // get the DataTable reference once
+
+                foreach (DataRow dr in dtObject.Rows)
+                {
+                    // extract all fields of the current row
+                    int chargeId = Convert.ToInt32(dr["chargeId"]);
+                    string description = dr["description"].ToString();
+                    int amountDollars = Convert.ToInt32(dr["amountDollars"]);
+                    int amountCents = Convert.ToInt32(dr["amountCents"]);
+                    DateTime dateCharged = Convert.ToDateTime(dr["dateCharged"]);
+                    DateTime dateDue = Convert.ToDateTime(dr["dateDue"]);
+                    int procedureId = Convert.ToInt32(dr["procedureId"]);
+                    int officeStaffId = Convert.ToInt32(dr["officeStaffId"]);
+                    int patientId = Convert.ToInt32(dr["patientId"]);
+                    int caseId = Convert.ToInt32(dr["caseId"]);
+
+                    // fill the ItemList
+                    Charges charge = new Charges();
+                    charge.chargeId = chargeId;
+                    charge.description = description;
+                    charge.amountDollars = amountDollars;
+                    charge.amountCents = amountCents;
+                    charge.dateCharged = dateCharged;
+                    charge.dateDue = dateDue;
+                    charge.procedureId = procedureId;
+                    charge.officeStaffId = officeStaffId;
+                    charge.patientId = patientId;
+                    charge.caseId = caseId;
+
+                    ItemList.Add(charge);
+                } // for
+            } // if
+        } // ReadList
+
+        /// <summary>
+        /// Read a sinlge record from the table and save the record in the
+        /// ItemList as an admission object.
+        /// </summary>
+        public void ReadListById(int inputChargeId)
+        {
+            ItemList.Clear();   // ensure that the itemlist is empty so we don't get duplicates
+
+            string myQuery = "SELECT * FROM " + theTable + " WHERE chargeId = " + "'" + inputChargeId + "'";
+
+            DataSet dsObject = QueryExecutor.ExecuteSqlQuery(myQuery);
+
+            if (dsObject != null && dsObject.Tables[0].Rows.Count > 0)
+            {
+                DataTable dtObject = dsObject.Tables[0];    // get the DataTable reference once
+
+                foreach (DataRow dr in dtObject.Rows)
+                {
+                    // extract all fields of the current row
+                    int chargeId = Convert.ToInt32(dr["chargeId"]);
+                    string description = dr["description"].ToString();
+                    int amountDollars = Convert.ToInt32(dr["amountDollars"]);
+                    int amountCents = Convert.ToInt32(dr["amountCents"]);
+                    DateTime dateCharged = Convert.ToDateTime(dr["dateCharged"]);
+                    DateTime dateDue = Convert.ToDateTime(dr["dateDue"]);
+                    int procedureId = Convert.ToInt32(dr["procedureId"]);
+                    int officeStaffId = Convert.ToInt32(dr["officeStaffId"]);
+                    int patientId = Convert.ToInt32(dr["patientId"]);
+                    int caseId = Convert.ToInt32(dr["caseId"]);
+
+                    // fill the ItemList
+                    Charges charge = new Charges();
+                    charge.chargeId = chargeId;
+                    charge.description = description;
+                    charge.amountDollars = amountDollars;
+                    charge.amountCents = amountCents;
+                    charge.dateCharged = dateCharged;
+                    charge.dateDue = dateDue;
+                    charge.procedureId = procedureId;
+                    charge.officeStaffId = officeStaffId;
+                    charge.patientId = patientId;
+                    charge.caseId = caseId;
+
+                    ItemList.Add(charge);
+                } // for
+            } // if
+        } // ReadList
+
+        /// <summary>
+        /// Given a single admission object, update the record correspnoding
+        /// to the object's admission id with any discrepancies in the admission
+        /// object.
+        /// </summary>
+        /// <param name="updatedCharge"></param>
+        public void UpdateItem(Charges updatedCharge)
+        {
+            using (SqlConnection myConnection = ConnectionsManager.GetNewConnection())
+            {
+                string myQuery = "UPDATE " + theTable +
+                    " SET " +
+                    "description = @description, " +
+                    "amountDollars = @amountDollars, " +
+                    "amountCents = @amountCents, " +
+                    "dateCharged = @dateCharged, " +
+                    "dateDue = @dateDue, " +
+                    "procedureId = @procedureId, " +
+                    "officeStaffId = @officeStaffId, " +
+                    "patientId = @patientId, " +
+                    "caseId = @caseId " +
+                    "WHERE " +
+                    "chargeId = @chargeId";
+
+                SqlCommand myCommand = new SqlCommand(myQuery, myConnection);
+
+                myCommand.Parameters.AddWithValue("@description", updatedCharge.description);
+                myCommand.Parameters.AddWithValue("@amountDollars", updatedCharge.amountDollars);
+                myCommand.Parameters.AddWithValue("@amountCents", updatedCharge.amountCents);
+                myCommand.Parameters.AddWithValue("@dateCharged", updatedCharge.dateCharged);
+                myCommand.Parameters.AddWithValue("@dateDue", updatedCharge.dateDue);
+                myCommand.Parameters.AddWithValue("@procedureId", updatedCharge.procedureId);
+                myCommand.Parameters.AddWithValue("@officeStaffId", updatedCharge.officeStaffId);
+                myCommand.Parameters.AddWithValue("@patientId", updatedCharge.patientId);
+                myCommand.Parameters.AddWithValue("@caseId", updatedCharge.caseId);
+                myCommand.Parameters.AddWithValue("@chargeId", updatedCharge.chargeId);
+
+                myCommand.ExecuteNonQuery();
+
+                myConnection.Close();
+            } // using
+        } // UpdateItem
+
+        /// <summary>
+        /// Given a list of admission objects, update their properties to the database
+        /// by admission id.
+        /// </summary>
+        public void UpdateList()
+        {
+            foreach (var charge in ItemList)
+            {
+                UpdateItem(charge);
+            }
+        }
+
+        /// <summary>
+        /// Insert a single admission record into the database.
+        /// </summary>
+        public void WriteItem(Charges newCharge)
+        {
+            using (SqlConnection myConnection = ConnectionsManager.GetNewConnection())
+            {
+                string myQuery = "INSERT INTO " + theTable +
+                    " (chargeId, description, amountDollars, amountCents, dateCharged, " +
+                    "dateDue, procedureId, officeStaffId, patientId, caseId)" +
+                    "VALUES (@chargeId, @description, @amountDollars, @amountCents, @dateCharged, " +
+                    "@dateDue, @procedureId, @officeStaffId, @patientId, @caseId)";
+
+                SqlCommand myCommand = new SqlCommand(myQuery, myConnection);
+
+                myCommand.Parameters.AddWithValue("@chargeId", newCharge.chargeId);
+                myCommand.Parameters.AddWithValue("@description", newCharge.description);
+                myCommand.Parameters.AddWithValue("@amountDollars", newCharge.amountDollars);
+                myCommand.Parameters.AddWithValue("@amountCents", newCharge.amountCents);
+                myCommand.Parameters.AddWithValue("@dateCharged", newCharge.dateCharged);
+                myCommand.Parameters.AddWithValue("@dateDue", newCharge.dateDue);
+                myCommand.Parameters.AddWithValue("@procedureId", newCharge.procedureId);
+                myCommand.Parameters.AddWithValue("@officeStaffId", newCharge.officeStaffId);
+                myCommand.Parameters.AddWithValue("@patientId", newCharge.patientId);
+                myCommand.Parameters.AddWithValue("@caseId", newCharge.caseId);
+
+                myCommand.ExecuteNonQuery();
+
+                myConnection.Close();
+            } // using
+        } // WriteItem
+
+        /// <summary>
+        /// Insert a list of admission records into the database.
+        /// </summary>
+        public void WriteList()
+        {
+            foreach (var charge in ItemList)
+            {
+                WriteItem(charge);
+            } // foreach
+        } // writelist
     }
 }
