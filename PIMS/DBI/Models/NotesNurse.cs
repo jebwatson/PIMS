58c5a01f6f01d0688974309089121c5faa4375b3
jnmorrow <jnm0014@uah.edu>
2017-02-12 04:51:11 -0800

    I implemented the table models for NotesDoctor, NotesNurse, NotesOfficeStaff, and NotesVolunteer

--- PIMS/DBI/Models/NotesNurse.cs
+++ PIMS/DBI/Models/NotesNurse.cs
@@ -6,5 +6,30 @@ namespace DBI
 {
     class NotesNurse
     {
+        const int _unassigned = -1;
+
+        public int notesId { get; set; }
+        public string notes { get; set; }
+        public int nurseId { get; set; }
+        public int patientId { get; set; }
+        public int caseId { get; set; }
+
+        public NotesNurse()
+        {
+            notesId = _unassigned;
+            notes = string.Empty;
+            nurseId = _unassigned;
+            patientId = _unassigned;
+            caseId = _unassigned;
+        }
+
+        public NotesNurse(int notesId, string notes, int nurseId, int patientId, int caseId)
+        {
+            this.notesId = notesId;
+            this.notes = notes;
+            this.nurseId = nurseId;
+            this.patientId = patientId;
+            this.caseId = caseId;
+        }
     }
 }
--- PIMS/DBI/Models/NotesOfficeStaff.cs
+++ PIMS/DBI/Models/NotesOfficeStaff.cs
@@ -6,5 +6,30 @@ namespace DBI
 {
     class NotesOfficeStaff
     {
+        const int _unassigned = -1;
+
+        public int notesId { get; set; }
+        public string notes { get; set; }
+        public int officeStaffId { get; set; }
+        public int patientId { get; set; }
+        public int caseId { get; set; }
+
+        public NotesOfficeStaff()
+        {
+            notesId = _unassigned;
+            notes = string.Empty;
+            officeStaffId = _unassigned;
+            patientId = _unassigned;
+            caseId = _unassigned;
+        }
+
+        public NotesOfficeStaff(int notesId, string notes, int officeStaffId, int patientId, int caseId)
+        {
+            this.notesId = notesId;
+            this.notes = notes;
+            this.officeStaffId = officeStaffId;
+            this.patientId = patientId;
+            this.caseId = caseId;
+        }
     }
 }
