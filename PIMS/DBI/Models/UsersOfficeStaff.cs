045bac5bad5c30515fc672f11d611c1e4622d6d5
DBurcham <dburcham76@gmail.com>
2017-02-21 18:23:27 -0600

    Completed model code for Users---.cs.
    Need review over UsersDoctorTable.cs to determine the issue with readlist

--- PIMS/DBI/Models/UsersOfficeStaff.cs
+++ PIMS/DBI/Models/UsersOfficeStaff.cs
@@ -6,5 +6,32 @@ namespace DBI
 {
     public class UsersOfficeStaff
     {
+        private int _unassigned = -1;
+
+        public int userId { get; set; }
+        public string nameLast { get; set; }
+        public string nameFirst { get; set; }
+        public string title { get; set; }
+        public string accessLevel { get; set; }
+
+        public UsersOfficeStaff()
+        {
+            userId = _unassigned;
+            nameLast = string.Empty;
+            nameFirst = string.Empty;
+            title = string.Empty;
+            accessLevel = string.Empty;
+        }
+
+        public UsersOfficeStaff(int id, string nameLast, string nameFirst, string title, string accessLevel)
+        {
+            userId = id;
+            this.nameLast = nameLast;
+            this.nameFirst = nameFirst;
+            this.title = title;
+            this.accessLevel = accessLevel;
+
+        }
     }
+
 }
