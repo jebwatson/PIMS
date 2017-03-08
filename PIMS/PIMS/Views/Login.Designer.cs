73bf4d68565854f89f65fae37072ee62e5423d9b
DBurcham <dburcham76@gmail.com>
2017-03-08 14:09:04 -0600

    Updated Login
    Fixed text size, entry box size

--- PIMS/PIMS/Views/Login.Designer.cs
+++ PIMS/PIMS/Views/Login.Designer.cs
@@ -36,34 +36,38 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(434, 140);
+            this.textBox1.Location = new System.Drawing.Point(434, 142);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(145, 22);
+            this.textBox1.Size = new System.Drawing.Size(209, 47);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(338, 140);
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(232, 142);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.Size = new System.Drawing.Size(196, 59);
             this.label1.TabIndex = 1;
             this.label1.Text = "Username";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(338, 210);
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(216, 221);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.Size = new System.Drawing.Size(192, 47);
             this.label2.TabIndex = 2;
             this.label2.Text = "Password";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(434, 210);
+            this.textBox2.Location = new System.Drawing.Point(434, 221);
+            this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(145, 22);
+            this.textBox2.Size = new System.Drawing.Size(209, 47);
             this.textBox2.TabIndex = 3;
             // 
             // Login
--- PIMS/PIMS/Views/Login.cs
+++ PIMS/PIMS/Views/Login.cs
@@ -16,5 +16,10 @@ namespace PIMS.Views
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
