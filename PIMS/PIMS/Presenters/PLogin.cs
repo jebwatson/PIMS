405589cfb410486ae9ec16fcb388cd2144a815db
Jeb Watson <jeb.watson@cepedasystems.com>
2017-04-11 16:24:21 -0500

    Updated login process
    Fix for login bugs

--- PIMS/PIMS/Presenters/PLogin.cs
+++ PIMS/PIMS/Presenters/PLogin.cs
@@ -24,18 +24,28 @@ namespace PIMS.Presenters
         /// </summary>
         public void Login()
         {
-            try {
-                ConnectionsManager.SQLServerConnectionString = "Data Source=DESKTOP-LCTG0FT\\SQLEXPRESS;Initial Catalog=" +
+            try
+            {
+                ConnectionsManager.SQLServerConnectionString = "Data Source=CSSA-JEB\\SQLEXPRESS;Initial Catalog=" +
                     "PIMSDev;Integrated Security=False;User Id=" + view.Username + ";Password=" + view.Password + ";MultipleActiveResultSets=True;";
 
                 // Establish a connection and close at the end of using
-                using (SqlConnection myConnection = ConnectionsManager.GetNewConnection()) {
-                    // Close the view and pass control back to the calling function.
-                    view.DialogResult = DialogResult.OK;
-                    view.Close();
+                using (SqlConnection myConnection = ConnectionsManager.GetNewConnection())
+                {
+                    if (myConnection != null)
+                    {
+                        // Close the view and pass control back to the calling function.
+                        view.DialogResult = DialogResult.OK;
+                        view.Close();
+                    }
+                    else
+                    {
+                        throw new Exception("Bad database connection.");
+                    }
                 }
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 MessageBox.Show(ex.Message);
             }
         }
