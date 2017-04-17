ee199719625464c3c4319314f34342f396e72d68
DBurcham <dburcham76@gmail.com>
2017-04-17 10:44:14 -0500

    Prescriptions View Progress
    Still working on it, but here is the view for review

---
+++ PIMS/PIMS/Presenters/PPrescriptions.cs
@@ -0,0 +1,29 @@
+﻿using DBI;
+using DBI.Utilities;
+using PIMS.Views;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIMS.Presenters
+{
+    class PPrescriptions
+    {
+        // Locals
+        Prescriptions View;
+        PrescriptionsTable MyPrescriptionsTable;
+        List<Prescriptions> MyPrescriptionsList;
+
+        public PPrescriptions(Prescriptions view)
+        {
+            View = view;
+        }
+
+       
+    }
+}
---
+++ PIMS/PIMS/Views/Perscriptions.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PIMS.Views
+{
+    partial class Perscriptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // objectListView1
+            // 
+            this.objectListView1.CellEditUseWholeCell = false;
+            this.objectListView1.Location = new System.Drawing.Point(135, 104);
+            this.objectListView1.Name = "objectListView1";
+            this.objectListView1.Size = new System.Drawing.Size(760, 261);
+            this.objectListView1.TabIndex = 0;
+            this.objectListView1.UseCompatibleStateImageBehavior = false;
+            this.objectListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Perscriptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 609);
+            this.Controls.Add(this.objectListView1);
+            this.Name = "Perscriptions";
+            this.Text = "Perscriptions";
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.ObjectListView objectListView1;
+    }
+}
