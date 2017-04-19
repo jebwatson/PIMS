1ef531c68a789408737c54c4fad663d57cf88b68
Jasper Morrow <jnm0014@uah.edu>
2017-04-19 11:32:23 -0700

    Procedures and Prescriptions

---
+++ PIMS/PIMS/Views/AddPrescription.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace PIMS.Views
+{
+    partial class AddPrescription
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prescriptionNameText = new System.Windows.Forms.TextBox();
+            this.prescriptionIdText = new System.Windows.Forms.TextBox();
+            this.patientIdText = new System.Windows.Forms.TextBox();
+            this.prescriptionAmountText = new System.Windows.Forms.TextBox();
+            this.prescriptionDateText = new System.Windows.Forms.TextBox();
+            this.prescriptionDurationText = new System.Windows.Forms.TextBox();
+            this.submitPrescription = new System.Windows.Forms.Button();
+            this.prescriptionName = new System.Windows.Forms.Label();
+            this.presctiptionId = new System.Windows.Forms.Label();
+            this.patientId = new System.Windows.Forms.Label();
+            this.prescriptionAmount = new System.Windows.Forms.Label();
+            this.prescriptionDate = new System.Windows.Forms.Label();
+            this.prescriptionDuration = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prescriptionNameText
+            // 
+            this.prescriptionNameText.Location = new System.Drawing.Point(33, 66);
+            this.prescriptionNameText.Name = "prescriptionNameText";
+            this.prescriptionNameText.Size = new System.Drawing.Size(145, 20);
+            this.prescriptionNameText.TabIndex = 0;
+            // 
+            // prescriptionIdText
+            // 
+            this.prescriptionIdText.Location = new System.Drawing.Point(213, 66);
+            this.prescriptionIdText.Name = "prescriptionIdText";
+            this.prescriptionIdText.Size = new System.Drawing.Size(145, 20);
+            this.prescriptionIdText.TabIndex = 1;
+            // 
+            // patientIdText
+            // 
+            this.patientIdText.Location = new System.Drawing.Point(389, 66);
+            this.patientIdText.Name = "patientIdText";
+            this.patientIdText.Size = new System.Drawing.Size(145, 20);
+            this.patientIdText.TabIndex = 2;
+            // 
+            // prescriptionAmountText
+            // 
+            this.prescriptionAmountText.Location = new System.Drawing.Point(33, 157);
+            this.prescriptionAmountText.Name = "prescriptionAmountText";
+            this.prescriptionAmountText.Size = new System.Drawing.Size(145, 20);
+            this.prescriptionAmountText.TabIndex = 3;
+            // 
+            // prescriptionDateText
+            // 
+            this.prescriptionDateText.Location = new System.Drawing.Point(213, 157);
+            this.prescriptionDateText.Name = "prescriptionDateText";
+            this.prescriptionDateText.Size = new System.Drawing.Size(145, 20);
+            this.prescriptionDateText.TabIndex = 4;
+            // 
+            // prescriptionDurationText
+            // 
+            this.prescriptionDurationText.Location = new System.Drawing.Point(389, 157);
+            this.prescriptionDurationText.Name = "prescriptionDurationText";
+            this.prescriptionDurationText.Size = new System.Drawing.Size(145, 20);
+            this.prescriptionDurationText.TabIndex = 5;
+            // 
+            // submitPrescription
+            // 
+            this.submitPrescription.Location = new System.Drawing.Point(409, 204);
+            this.submitPrescription.Name = "submitPrescription";
+            this.submitPrescription.Size = new System.Drawing.Size(109, 53);
+            this.submitPrescription.TabIndex = 6;
+            this.submitPrescription.Text = "Submit";
+            this.submitPrescription.UseVisualStyleBackColor = true;
+            // 
+            // prescriptionName
+            // 
+            this.prescriptionName.AutoSize = true;
+            this.prescriptionName.Location = new System.Drawing.Point(59, 50);
+            this.prescriptionName.Name = "prescriptionName";
+            this.prescriptionName.Size = new System.Drawing.Size(93, 13);
+            this.prescriptionName.TabIndex = 7;
+            this.prescriptionName.Text = "Prescription Name";
+            // 
+            // presctiptionId
+            // 
+            this.presctiptionId.AutoSize = true;
+            this.presctiptionId.Location = new System.Drawing.Point(245, 50);
+            this.presctiptionId.Name = "presctiptionId";
+            this.presctiptionId.Size = new System.Drawing.Size(76, 13);
+            this.presctiptionId.TabIndex = 8;
+            this.presctiptionId.Text = "Prescription ID";
+            // 
+            // patientId
+            // 
+            this.patientId.AutoSize = true;
+            this.patientId.Location = new System.Drawing.Point(439, 50);
+            this.patientId.Name = "patientId";
+            this.patientId.Size = new System.Drawing.Size(54, 13);
+            this.patientId.TabIndex = 9;
+            this.patientId.Text = "Patient ID";
+            // 
+            // prescriptionAmount
+            // 
+            this.prescriptionAmount.AutoSize = true;
+            this.prescriptionAmount.Location = new System.Drawing.Point(82, 141);
+            this.prescriptionAmount.Name = "prescriptionAmount";
+            this.prescriptionAmount.Size = new System.Drawing.Size(43, 13);
+            this.prescriptionAmount.TabIndex = 10;
+            this.prescriptionAmount.Text = "Amount";
+            // 
+            // prescriptionDate
+            // 
+            this.prescriptionDate.AutoSize = true;
+            this.prescriptionDate.Location = new System.Drawing.Point(245, 141);
+            this.prescriptionDate.Name = "prescriptionDate";
+            this.prescriptionDate.Size = new System.Drawing.Size(88, 13);
+            this.prescriptionDate.TabIndex = 11;
+            this.prescriptionDate.Text = "Prescription Date";
+            // 
+            // prescriptionDuration
+            // 
+            this.prescriptionDuration.AutoSize = true;
+            this.prescriptionDuration.Location = new System.Drawing.Point(413, 141);
+            this.prescriptionDuration.Name = "prescriptionDuration";
+            this.prescriptionDuration.Size = new System.Drawing.Size(105, 13);
+            this.prescriptionDuration.TabIndex = 12;
+            this.prescriptionDuration.Text = "Prescription Duration";
+            // 
+            // AddPrescription
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 269);
+            this.Controls.Add(this.prescriptionDuration);
+            this.Controls.Add(this.prescriptionDate);
+            this.Controls.Add(this.prescriptionAmount);
+            this.Controls.Add(this.patientId);
+            this.Controls.Add(this.presctiptionId);
+            this.Controls.Add(this.prescriptionName);
+            this.Controls.Add(this.submitPrescription);
+            this.Controls.Add(this.prescriptionDurationText);
+            this.Controls.Add(this.prescriptionDateText);
+            this.Controls.Add(this.prescriptionAmountText);
+            this.Controls.Add(this.patientIdText);
+            this.Controls.Add(this.prescriptionIdText);
+            this.Controls.Add(this.prescriptionNameText);
+            this.Name = "AddPrescription";
+            this.Text = "AddPrescription";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox prescriptionNameText;
+        private System.Windows.Forms.TextBox prescriptionIdText;
+        private System.Windows.Forms.TextBox patientIdText;
+        private System.Windows.Forms.TextBox prescriptionAmountText;
+        private System.Windows.Forms.TextBox prescriptionDateText;
+        private System.Windows.Forms.TextBox prescriptionDurationText;
+        private System.Windows.Forms.Button submitPrescription;
+        private System.Windows.Forms.Label prescriptionName;
+        private System.Windows.Forms.Label presctiptionId;
+        private System.Windows.Forms.Label patientId;
+        private System.Windows.Forms.Label prescriptionAmount;
+        private System.Windows.Forms.Label prescriptionDate;
+        private System.Windows.Forms.Label prescriptionDuration;
+    }
+}
---
+++ PIMS/PIMS/Views/AddProcedure.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace PIMS.Views
+{
+    partial class AddProcedure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.procedureIdText = new System.Windows.Forms.TextBox();
+            this.procedureTypeText = new System.Windows.Forms.TextBox();
+            this.patientIdText = new System.Windows.Forms.TextBox();
+            this.doctorIdText = new System.Windows.Forms.TextBox();
+            this.startTimeText = new System.Windows.Forms.TextBox();
+            this.stopTimeText = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.durationMinutesText = new System.Windows.Forms.TextBox();
+            this.procedureNotesText = new System.Windows.Forms.TextBox();
+            this.submitProcedure = new System.Windows.Forms.Button();
+            this.procedureId = new System.Windows.Forms.Label();
+            this.procedureType = new System.Windows.Forms.Label();
+            this.patientId = new System.Windows.Forms.Label();
+            this.doctorId = new System.Windows.Forms.Label();
+            this.startTime = new System.Windows.Forms.Label();
+            this.stopTime = new System.Windows.Forms.Label();
+            this.durationHours = new System.Windows.Forms.Label();
+            this.durationMinutes = new System.Windows.Forms.Label();
+            this.procedureNotes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // procedureIdText
+            // 
+            this.procedureIdText.Location = new System.Drawing.Point(36, 35);
+            this.procedureIdText.Name = "procedureIdText";
+            this.procedureIdText.Size = new System.Drawing.Size(197, 20);
+            this.procedureIdText.TabIndex = 0;
+            // 
+            // procedureTypeText
+            // 
+            this.procedureTypeText.Location = new System.Drawing.Point(316, 35);
+            this.procedureTypeText.Name = "procedureTypeText";
+            this.procedureTypeText.Size = new System.Drawing.Size(200, 20);
+            this.procedureTypeText.TabIndex = 1;
+            // 
+            // patientIdText
+            // 
+            this.patientIdText.Location = new System.Drawing.Point(316, 91);
+            this.patientIdText.Name = "patientIdText";
+            this.patientIdText.Size = new System.Drawing.Size(200, 20);
+            this.patientIdText.TabIndex = 2;
+            // 
+            // doctorIdText
+            // 
+            this.doctorIdText.Location = new System.Drawing.Point(36, 91);
+            this.doctorIdText.Name = "doctorIdText";
+            this.doctorIdText.Size = new System.Drawing.Size(197, 20);
+            this.doctorIdText.TabIndex = 3;
+            // 
+            // startTimeText
+            // 
+            this.startTimeText.Location = new System.Drawing.Point(36, 145);
+            this.startTimeText.Name = "startTimeText";
+            this.startTimeText.Size = new System.Drawing.Size(197, 20);
+            this.startTimeText.TabIndex = 4;
+            // 
+            // stopTimeText
+            // 
+            this.stopTimeText.Location = new System.Drawing.Point(319, 145);
+            this.stopTimeText.Name = "stopTimeText";
+            this.stopTimeText.Size = new System.Drawing.Size(197, 20);
+            this.stopTimeText.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(319, 201);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(197, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // durationMinutesText
+            // 
+            this.durationMinutesText.Location = new System.Drawing.Point(36, 201);
+            this.durationMinutesText.Name = "durationMinutesText";
+            this.durationMinutesText.Size = new System.Drawing.Size(197, 20);
+            this.durationMinutesText.TabIndex = 7;
+            // 
+            // procedureNotesText
+            // 
+            this.procedureNotesText.Location = new System.Drawing.Point(36, 259);
+            this.procedureNotesText.Multiline = true;
+            this.procedureNotesText.Name = "procedureNotesText";
+            this.procedureNotesText.Size = new System.Drawing.Size(375, 130);
+            this.procedureNotesText.TabIndex = 8;
+            // 
+            // submitProcedure
+            // 
+            this.submitProcedure.Location = new System.Drawing.Point(420, 329);
+            this.submitProcedure.Name = "submitProcedure";
+            this.submitProcedure.Size = new System.Drawing.Size(96, 60);
+            this.submitProcedure.TabIndex = 9;
+            this.submitProcedure.Text = "Submit";
+            this.submitProcedure.UseVisualStyleBackColor = true;
+            // 
+            // procedureId
+            // 
+            this.procedureId.AutoSize = true;
+            this.procedureId.Location = new System.Drawing.Point(35, 19);
+            this.procedureId.Name = "procedureId";
+            this.procedureId.Size = new System.Drawing.Size(70, 13);
+            this.procedureId.TabIndex = 10;
+            this.procedureId.Text = "Procedure ID";
+            // 
+            // procedureType
+            // 
+            this.procedureType.AutoSize = true;
+            this.procedureType.Location = new System.Drawing.Point(316, 19);
+            this.procedureType.Name = "procedureType";
+            this.procedureType.Size = new System.Drawing.Size(83, 13);
+            this.procedureType.TabIndex = 11;
+            this.procedureType.Text = "Procedure Type";
+            // 
+            // patientId
+            // 
+            this.patientId.AutoSize = true;
+            this.patientId.Location = new System.Drawing.Point(316, 75);
+            this.patientId.Name = "patientId";
+            this.patientId.Size = new System.Drawing.Size(54, 13);
+            this.patientId.TabIndex = 12;
+            this.patientId.Text = "Patient ID";
+            // 
+            // doctorId
+            // 
+            this.doctorId.AutoSize = true;
+            this.doctorId.Location = new System.Drawing.Point(35, 75);
+            this.doctorId.Name = "doctorId";
+            this.doctorId.Size = new System.Drawing.Size(53, 13);
+            this.doctorId.TabIndex = 13;
+            this.doctorId.Text = "Doctor ID";
+            // 
+            // startTime
+            // 
+            this.startTime.AutoSize = true;
+            this.startTime.Location = new System.Drawing.Point(33, 129);
+            this.startTime.Name = "startTime";
+            this.startTime.Size = new System.Drawing.Size(55, 13);
+            this.startTime.TabIndex = 14;
+            this.startTime.Text = "Start Time";
+            // 
+            // stopTime
+            // 
+            this.stopTime.AutoSize = true;
+            this.stopTime.Location = new System.Drawing.Point(319, 129);
+            this.stopTime.Name = "stopTime";
+            this.stopTime.Size = new System.Drawing.Size(51, 13);
+            this.stopTime.TabIndex = 15;
+            this.stopTime.Text = "Stop time";
+            // 
+            // durationHours
+            // 
+            this.durationHours.AutoSize = true;
+            this.durationHours.Location = new System.Drawing.Point(33, 185);
+            this.durationHours.Name = "durationHours";
+            this.durationHours.Size = new System.Drawing.Size(84, 13);
+            this.durationHours.TabIndex = 16;
+            this.durationHours.Text = "Duration (Hours)";
+            // 
+            // durationMinutes
+            // 
+            this.durationMinutes.AutoSize = true;
+            this.durationMinutes.Location = new System.Drawing.Point(319, 185);
+            this.durationMinutes.Name = "durationMinutes";
+            this.durationMinutes.Size = new System.Drawing.Size(93, 13);
+            this.durationMinutes.TabIndex = 17;
+            this.durationMinutes.Text = "Duration (Minutes)";
+            // 
+            // procedureNotes
+            // 
+            this.procedureNotes.AutoSize = true;
+            this.procedureNotes.Location = new System.Drawing.Point(35, 243);
+            this.procedureNotes.Name = "procedureNotes";
+            this.procedureNotes.Size = new System.Drawing.Size(87, 13);
+            this.procedureNotes.TabIndex = 18;
+            this.procedureNotes.Text = "Procedure Notes";
+            // 
+            // AddProcedure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 416);
+            this.Controls.Add(this.procedureNotes);
+            this.Controls.Add(this.durationMinutes);
+            this.Controls.Add(this.durationHours);
+            this.Controls.Add(this.stopTime);
+            this.Controls.Add(this.startTime);
+            this.Controls.Add(this.doctorId);
+            this.Controls.Add(this.patientId);
+            this.Controls.Add(this.procedureType);
+            this.Controls.Add(this.procedureId);
+            this.Controls.Add(this.submitProcedure);
+            this.Controls.Add(this.procedureNotesText);
+            this.Controls.Add(this.durationMinutesText);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.stopTimeText);
+            this.Controls.Add(this.startTimeText);
+            this.Controls.Add(this.doctorIdText);
+            this.Controls.Add(this.patientIdText);
+            this.Controls.Add(this.procedureTypeText);
+            this.Controls.Add(this.procedureIdText);
+            this.Name = "AddProcedure";
+            this.Text = "AddProcedure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox procedureIdText;
+        private System.Windows.Forms.TextBox procedureTypeText;
+        private System.Windows.Forms.TextBox patientIdText;
+        private System.Windows.Forms.TextBox doctorIdText;
+        private System.Windows.Forms.TextBox startTimeText;
+        private System.Windows.Forms.TextBox stopTimeText;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox durationMinutesText;
+        private System.Windows.Forms.TextBox procedureNotesText;
+        private System.Windows.Forms.Button submitProcedure;
+        private System.Windows.Forms.Label procedureId;
+        private System.Windows.Forms.Label procedureType;
+        private System.Windows.Forms.Label patientId;
+        private System.Windows.Forms.Label doctorId;
+        private System.Windows.Forms.Label startTime;
+        private System.Windows.Forms.Label stopTime;
+        private System.Windows.Forms.Label durationHours;
+        private System.Windows.Forms.Label durationMinutes;
+        private System.Windows.Forms.Label procedureNotes;
+    }
+}
