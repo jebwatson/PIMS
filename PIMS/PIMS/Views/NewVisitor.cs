f328a16afcc2c492ce0ba2926d29a3dad25f4f82
Jasper Morrow <jnm0014@uah.edu>
2017-04-29 19:33:24 -0700

    Functionality of the Add Visitor and Procedure forms

--- PIMS/PIMS/Views/NewVisitor.cs
+++ PIMS/PIMS/Views/NewVisitor.cs
@@ -16,5 +16,30 @@ namespace PIMS.Views
         {
             InitializeComponent();
         }
+
+        //Properties
+        public string LastName { get { return this.lastName.Text; } }
+        public string FirstName { get { return this.firstName.Text; } }
+        public string Relation { get { return this.visitorRelation.Text; } }
+        public DateTime LastVisit { get { return this.lastVisit.Value.Date; } }
+        public int Id { get { return Convert.ToInt32(this.patientId.Text); } }
+
+        private void submitVisitors_Click(object sender, EventArgs e)
+        {
+            DBI.Visitors MyVisitor = new DBI.Visitors(LastName, FirstName, Relation, LastVisit, Id, 1);
+
+            DBI.VisitorsTable MyVisitorsTable = new DBI.VisitorsTable();
+
+            MyVisitorsTable.WriteItem(MyVisitor);
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void cancelVisitor_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
     }
 }
