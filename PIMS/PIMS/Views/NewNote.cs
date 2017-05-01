6745cbe6dab7a8eabc345cc33ced7b3283f153db
Jeb <jeb.watson@cepedasystems.com>
2017-05-01 13:43:33 -0500

    partial

--- PIMS/PIMS/Views/NewNote.cs
+++ PIMS/PIMS/Views/NewNote.cs
@@ -12,7 +12,7 @@ namespace PIMS.Views
 {
     public partial class NewNote : Form
     {
-        public NewNote()
+        public NewNote(DBI.Notes CurrentNote)
         {
             InitializeComponent();
         }
